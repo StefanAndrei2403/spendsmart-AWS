44b46bcb395c2ff034f711ff4b7b95a2f096b73e
StefanAndrei2403 <stefanbaizat@gmail.com>
2025-04-02 22:48:04 +0300

    1.0.37

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import reportWebVitals from './reportWebVitals';
+
 
 // Înlocuiește cu client ID-ul tău
 const CLIENT_ID = '166245198945-gh14hvgqlcrr58re9rjdqu985srlnnvo.apps.googleusercontent.com';
